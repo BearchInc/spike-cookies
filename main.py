51ecef1dfd94844e53b0cf2ca79a95195273b870
lisardo <lisardo.kist@gmail.com>
2015-10-21 17:57:11 -0200

    [#]

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ class Github:
     def login(self, user, password):
         result = urlfetch.fetch(Github.loginURL)
         loginSetCookiesHeader = result.headers['Set-Cookie']
-        print('login cookie: ' + loginSetCookiesHeader)
         doc = BeautifulSoup(result.content, "html.parser")
         token = doc.find('input', {'name': 'authenticity_token'}).attrs['value']
         print(token)
@@ -39,10 +38,35 @@ class Github:
 class Facebook:
     loginURL = 'https://www.facebook.com/login.php?login_attempt=1&lwv=110'
     def login(self, user, password):
-        cookie = Cookie.SimpleCookie()
         data = urllib.urlencode({'email': user, 'pass': password})
-        result = urlfetch.fetch(url=Facebook.loginURL, payload=data, follow_redirects=False, method=urlfetch.POST, headers={ 'cookie':'_ga=BakerySSO'})
+        result = urlfetch.fetch(url=Facebook.loginURL, 
+                payload=data, 
+                method=urlfetch.POST, 
+                follow_redirects=False, 
+                headers={ 'cookie':'_ga=BakerySSO'})
         return readCookies(result.headers['Set-Cookie'])
+
+
+class Twitter:
+    loginURL = 'https://twitter.com'
+    sessionURL = 'https://twitter.com/sessions'
+
+    def login(self, user, password):
+            result = urlfetch.fetch(Twitter.loginURL)
+            loginSetCookiesHeader = result.headers['Set-Cookie']
+            doc = BeautifulSoup(result.content, "html.parser")
+            token = doc.find('input', {'name': 'authenticity_token'}).attrs['value']
+
+            data = urllib.urlencode({'authenticity_token': token, 'session[username_or_email]':user, 'session[password]': password})
+            result = urlfetch.fetch(url=Twitter.sessionURL, 
+                    payload=data, 
+                    method=urlfetch.POST, 
+                    follow_redirects=False, 
+                    headers={ 'Cookie': loginSetCookiesHeader})
+
+            return readCookies(result.headers['Set-Cookie'])
+
+
 
 
 app = Flask(__name__)
@@ -65,6 +89,14 @@ def login_github():
     cookies = Github().login('joaobearch', 'Unseen2015')
     return flask.jsonify(**{
         "system": "github",
+        "cookies": cookies
+    })
+
+@app.route("/login/twitter")
+def login_twitter():
+    cookies = Twitter().login('not_lisardo', '1lisa3do')
+    return flask.jsonify(**{
+        "system": "twitter",
         "cookies": cookies
     })
 
