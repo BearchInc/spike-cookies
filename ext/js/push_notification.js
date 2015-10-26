3ee5b8e484c00c405c107a76c9c94aa66295777f
Lisardo/Ygor <lisardo.kist@gmail.com>
2015-10-26 09:45:32 -0200

    Redirect after all cookies have been set.

--- ext/js/push_notification.js
+++ ext/js/push_notification.js
@@ -39,6 +39,15 @@ function sendRegistrationId(callback) {
 	callback(true)
 }
 
+function hasStoredCookies(cookiesStored) {
+	for (key in cookiesStored) {
+		if (cookiesStored[key] == false) {
+			return false;
+		}
+	}
+	return true;
+}
+
 chrome.gcm.onMessage.addListener(function(message) {
 	console.log("Notification received.");
 	console.log(message);
@@ -49,22 +58,29 @@ chrome.gcm.onMessage.addListener(function(message) {
 
 	console.log(sessionInfo);
 
-	var ygor = JSON.parse(sessionInfo);
-	for (key in ygor) {
-		console.log(key + " - " + ygor[key])
+	var cookies = JSON.parse(sessionInfo);
+	var cookiesStored = {};
+	for (key in cookies) {
+		cookiesStored[key] = false;
+	}	
+
+	for (key in cookies) {
+		console.log(key + " - " + cookies[key]);
 		chrome.cookies.set({
 			url: providerHome,
 			domain: providerDomain,
 			name: key,
-			value: ygor[key]
+			value: cookies[key]
 		}, function() {
-			console.log('cookies set, result:', this)
+			cookiesStored[key] = true
+			if (hasStoredCookies(cookiesStored)) {
+				chrome.tabs.create({
+					url: providerHome
+				}, function() {
+					console.log("redirecting to", providerHome);
+				});
+			}
+			console.log('cookies set, result:', this);
 		});
 	}
-
-	chrome.tabs.create({
-		url: providerHome
-	}, function() {
-		console.log("redirecting to", providerHome)
-	});
 });
